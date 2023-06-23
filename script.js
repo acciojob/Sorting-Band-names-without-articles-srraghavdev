44fc2e142ec2ec93b387b1209913872cde367dde
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-23 19:15:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
-console.log(bandNames.sort())
+bandNames.sort((a,b)=> (strip(a)>strip(b))? 1: -1)
 const ref= document.getElementsByTagName("ul")[0]
 for(let i=0;i<bandNames.length;i++){
 	const li = document.createElement("li") 
 	li.innerText= bandNames[i]
 	ref.append(li)
+}
+function strip(word){
+	let arr= word.split(" ")
+	let s=""
+	let articles=['a','A','an','AN','An','the','The','THE']
+	for(let i=0;i<arr.length;i++){
+		if(articles.indexOf(arr[i])=== -1){
+			s=s+arr[i]
+		}
+	}
+	return s.trim()
 }
