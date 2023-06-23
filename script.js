eee5485213064992c3fabc47f2b9ad20cc14dfde
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-23 18:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
-
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+console.log(bandNames.sort)
+cont ref= document.getElementsByTagName("ul")[0]
+for(let i=0;i<bandNames.length;i++){
+	const li = document.CreateElement("li")
+	li.innerText= bandNames[i]
+	ref.append(li)
+}
